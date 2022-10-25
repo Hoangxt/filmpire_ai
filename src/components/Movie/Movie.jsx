a84c17c321c6ba2bae973a979aef118b7e16bd4b
Hoangxt <dinhhuyhoangxt@gmail.com>
2022-10-25 14:12:47 +0700

    5. Finishing Movie View

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -7,10 +7,28 @@ import useStyles from "./styles";
 const Movie = ({ movie, i }) => {
   const classes = useStyles();
   return (
-    <Grid item sx={12} sm={6} md={4} lg={3} xl={2} className={classes.movies}>
-      <Typography className={classes.title} variant={5}>
-        {movie.title}
-      </Typography>
+    <Grid item sx={12} sm={6} md={4} lg={3} xl={2} className={classes.movie}>
+      <Grow in key={i} timeout={(i + 1) * 250}>
+        <Link className={classes.links} to={`/movie/${movie.id}`}>
+          <img
+            src={
+              movie.poster_path
+                ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                : "https://www.fillmurray.com/200/300"
+            }
+            alt={movie.title}
+            className={classes.image}
+          />
+          <Typography className={classes.title} variant={5}>
+            {movie.title}
+          </Typography>
+          <Tooltip disableTouchListener title={`${movie.vote_average} / 10`}>
+            <div>
+              <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+            </div>
+          </Tooltip>
+        </Link>
+      </Grow>
     </Grid>
   );
 };
