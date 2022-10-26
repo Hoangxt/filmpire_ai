96529e60667273ad9086ffa48cacef625eb3ce3e
Hoangxt <dinhhuyhoangxt@gmail.com>
2022-10-26 09:08:45 +0700

    13. Finalizing Authentication

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,44 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+// mui
+import { Box, Button, Typography } from "@mui/material";
+import { ExitToApp } from "@mui/icons-material";
+// get access to profile name or id from redux state
+// display in the profile component
+// redux
+import { useSelector } from "react-redux";
 const Profile = () => {
-  return <div>Profile</div>;
+  // get user from redux state
+  const { user } = useSelector((state) => state.user);
+
+  const favoriteMovies = [];
+  // logout function
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = "/";
+  };
+  return (
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp;
+          <ExitToApp />
+        </Button>
+      </Box>
+      {/* if no have favoriteMovies */}
+      {!favoriteMovies?.length ? (
+        <Typography variant="h5">
+          Add favorite movies or watch some movies to see them here{" "}
+        </Typography>
+      ) : (
+        <Box>
+          <Typography variant="h5">Favorite Movies</Typography>
+        </Box>
+      )}
+    </Box>
+  );
 };
 
 export default Profile;
